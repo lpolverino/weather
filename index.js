7edfd1f5815046f0a20eff57b161003f5af7ece1
lpolverino <lorenz.polverino@gmail.com>
2023-11-13 10:54:43 -0300

    retrieved fetch data

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 async function getLocationWeather(location) {
-    //we need to insert the location and check for error
+    const url ="https://api.weatherapi.com/v1/current.json?key=ce6352e5ca1c44c4917131930231211&q=" + location
     try{
-        response = await fetch("https://api.weatherapi.com/v1/current.json?key=ce6352e5ca1c44c4917131930231211&q=london")
-        console.log(response.json());
-        return response.json()
+        const response = await fetch("https://api.weatherapi.com/v1/current.json?key=ce6352e5ca1c44c4917131930231211&q=london")
+        const data = await response.json()
+        return data
     }
     catch(e){
         console.log(e);
     }
 }
- getLocationWeather("london")
+
+async function proccessInfo(info){
+    console.log(info);
+    const locationInfo = {
+        name: info.location.name,
+        region : info.location.region
+    }
+    const weather = {
+        description: info.current.condition.text,
+        feels:{
+            farenheit:info.current.feelslike_f,
+            celcius:info.current.feelslike_c
+        },
+        temperature:{
+            farenheit:info.current.feelslike_f,
+            celcius:info.current.feelslike_c
+        }
+    }
+}
+
+async function getInfo(location){
+    const info = await getLocationWeather(location)
+    await proccessInfo(info)
+}
+
+getInfo("london")
